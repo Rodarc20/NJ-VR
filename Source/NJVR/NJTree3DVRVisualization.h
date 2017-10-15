40b58bc1c7f4c3ddac37031cea3c4d7595384bc3
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2017-10-15 10:59:04 -0500

    Clase para NJ-Tree con algunas variaciones para el 3D

---
+++ Source/NJVR/NJTree3DVRVisualization.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRVisualization.h"
+#include "NJTree3DVRVisualization.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NJVR_API ANJTree3DVRVisualization : public AVRVisualization
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
