7a07d8d87da2ddd8806a2592c0af10e5245c898b
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2017-11-20 01:15:13 -0500

    nuevas clases para los layouts

---
+++ Source/NJVR/PolyPlaneVRVisualization.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRVisualization.h"
+#include "PolyPlaneVRVisualization.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NJVR_API APolyPlaneVRVisualization : public AVRVisualization
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/NJVR/SphericalTreeVRVisualization.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRVisualization.h"
+#include "SphericalTreeVRVisualization.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NJVR_API ASphericalTreeVRVisualization : public AVRVisualization
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
